f1bce9777b2d73092eb5f669f5d152c475f3fd66
Jeff Schwab <jeff@unbuggy.com>
2018-05-12 18:23:32 -0400

    Remove unnecessary function definition

--- tutorial/pong.py
+++ tutorial/pong.py
@@ -5,9 +5,6 @@ import sdl2.ext
 WHITE = sdl2.ext.Color(255, 255, 255)
 
 class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
-    def __init__(self, window):
-        super(SoftwareRenderer, self)__init__(window)
-
     def render(self, components):
         sdl2.ext.fill(self.surface, sdl2.ext.Color(0, 0, 0))
         super(SoftwareRenderer, self).render(components)
