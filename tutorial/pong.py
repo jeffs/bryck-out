2a3169c69efcedde0fba99adbb40f4f48b8aa81b
Jeff Schwab <jeff@unbuggy.com>
2018-05-13 03:22:06 -0400

    Improve bouncing
    Bounce off paddles at different angles.  Bounce off walls.

--- tutorial/pong.py
+++ tutorial/pong.py
@@ -36,6 +36,29 @@ class CollisionSystem(sdl2.ext.Applicator):
         if collitems:
             self.ball.velocity.vx = -self.ball.velocity.vx
 
+            sprite = collitems[0][1]
+            ballcentery = self.ball.sprite.y + self.ball.sprite.size[1] // 2
+            halfheight = sprite.size[1] // 2
+            stepsize = halfheight // 10
+            degrees = 0.7
+            paddlecentery = sprite.y + halfheight
+            if ballcentery < paddlecentery:
+                factor = (paddlecentery - ballcentery) // stepsize
+                self.ball.velocity.vy = -int(round(factor * degrees))
+            elif ballcentery > paddlecentery:
+                factor = (ballcentery - paddlecentery) // stepsize
+                self.ball.velocity.vy = int(round(factor * degrees))
+            else:
+                self.ball.velocity.vy = -self.ball.velocity.vy
+
+        if (self.ball.sprite.y <= self.miny or
+            self.ball.sprite.y + self.ball.sprite.size[1] >= self.maxy):
+            self.ball.velocity.vy = -self.ball.velocity.vy
+
+        if (self.ball.sprite.x <= self.minx or
+            self.ball.sprite.x + self.ball.sprite.size[0] >= self.maxx):
+            self.ball.velocity.vx = -self.ball.velocity.vx
+
 class MovementSystem(sdl2.ext.Applicator):
     def __init__(self, minx, miny, maxx, maxy):
         super().__init__()
